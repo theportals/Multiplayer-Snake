0647dfab6591b1de377d8cdd091c06d0417be4ce
theportals <clitchedglock94@gmail.com>
2024-04-18 10:50:33 -0600

    Remove obstacles

--- Shared/Util/Constants.cs
+++ Shared/Util/Constants.cs
@@ -3,7 +3,7 @@ namespace Shared.Util;
 public class Constants
 {
     public const int ARENA_SIZE = 1000;
-    public const int OBSTACLE_COUNT = 200;
+    public const int OBSTACLE_COUNT = 0;
     public const int FOOD_COUNT = 25;
     public const float squiggleFactor = 10;
     public const float antiSquiggleFactor = 10;
